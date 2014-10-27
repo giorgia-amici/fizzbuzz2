e5aa31d6ea52461a84f9defb114d87a0fec345e9
Giorgia <giorgia1london@gmail.com>
2014-10-27 16:00:15 +0000

    first lesson

--- lib/fizzbuzz2.rb
+++ lib/fizzbuzz2.rb
@@ -1,19 +1,19 @@
 
-class Fixnum
+# class Fixnum
 
-def self.fizz?(num)
+def fizz?(num)
 	num % 3 == 0
 end
 
-def self.buzz?(num)
+def buzz?(num)
 	num % 5 == 0
 end
 
-def self.fizzbuzz?(num)
+def fizzbuzz?(num)
 	return 'fizzbuzz' if fizz?(num) && buzz?(num)
 	return 'fizz' if fizz?(num)
 	return 'buzz' if buzz?(num)	
 	num
 end
 
-end
+# end
--- spec/fizzbuzz2_spec.rb
+++ spec/fizzbuzz2_spec.rb
@@ -1,21 +1,21 @@
 require 'fizzbuzz2'
 
-describe Fixnum do
+describe 'fizzbuzz' do
 
 	context 'it knows when a number is divisible by 3, 5 or both' do
 
 		it 'is divisible by 3' do
-		expect(Fixnum.fizz?(3)).to be true
+		expect(fizz?(3)).to be true
 		end
 
 
 		it 'is divisible by 5' do
-		expect(Fixnum.buzz?(5)).to be true
+		expect(buzz?(5)).to be true
 		end
 
 
 		it 'is divisible by 15' do
-		expect(Fixnum.fizzbuzz?(3) && Fixnum.buzz?(5)).to eq true
+		expect(fizzbuzz?(3) && buzz?(5)).to eq true
 		end
 
 	end
@@ -23,20 +23,20 @@ describe Fixnum do
 	context 'return fizz if no is divisible by 3, buzz if divisible by 5, fizzbuzz is divisible by both otherwise returns the number' do
 
 		it 'is fizz' do
-		expect(Fixnum.fizzbuzz?(3)).to eq('fizz')
+		expect(fizzbuzz?(3)).to eq('fizz')
 		end
 
 
 		it 'is buzz' do
-		expect(Fixnum.fizzbuzz?(5)).to eq('buzz')
+		expect(fizzbuzz?(5)).to eq('buzz')
 		end
 
 		it 'is fizzbuzz' do
-		expect(Fixnum.fizzbuzz?(15)).to eq('fizzbuzz')
+		expect(fizzbuzz?(15)).to eq('fizzbuzz')
 		end
 
 		it  'is number' do
-		expect(Fixnum.fizz?(7)).to be false
+		expect(fizz?(7)).to be false
 		end
 
 	end
